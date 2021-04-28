a28ddfb82a2ef4993b579b2cc893ca0bcbb6cbe8
Afarait Sulaimon Bola <afaraitsulaimon@gmail.com>
2021-04-28 08:57:46 +0100

    made changes to debug-to-true

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0jo3@v)s56esue!g+1wg+*4ycmm24ou92mz8&2ym&8i6zp_69d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
